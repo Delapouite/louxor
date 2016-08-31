14782a0f80cdd55df13f745977081d88c3495266
Delapouite <delapouite@gmail.com>
2016-08-31 15:06:22 +0200

    style(<CurrentSong>): adjust spacing and contrast

--- src/client/js/components/CurrentSong.js
+++ src/client/js/components/CurrentSong.js
@@ -6,17 +6,18 @@ import { openAlbums } from '../actions'
 class CurrentSong extends React.Component {
 	render () {
 		let { album, artist, title, date, track } = this.props.song
-		album = album !== 'singles' ? 'on ' + album : ''
-		date = date ? ' in ' + date : ''
 
 		return (
 			<div className="current-song">
-				<span className="current-song-title" title={track}>{title}</span>
-				<span className="current-song-artist">by&nbsp;
+				<div className="current-song-title" title={track}>{title}</div>
+				<div className="current-song-artist">
+					<span className="current-song-by">by</span>
 					<span onClick={() => {this.props.openAlbums()}}>{artist}</span>
-				</span>
-				<span className="current-song-album">{album}</span>
-				<span className="current-song-date">{date}</span>
+				</div>
+				{ album !== 'singles' ? <span className="current-song-on">on</span> : null }
+				{ album !== 'singles' ? <span className="current-song-album">{album}</span> : null }
+				<span className="current-song-in">in</span>
+				{ date ? <span className="current-song-date">{date}</span> : null }
 			</div>
 		)
 	}
