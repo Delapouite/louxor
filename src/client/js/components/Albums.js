dec41a84453e2e392f92c5e6453ffb1908c9e6e2
Delapouite <delapouite@gmail.com>
2016-09-01 12:26:44 +0200

    feat(client): add optional animation for <Album>

--- src/client/js/components/Albums.js
+++ src/client/js/components/Albums.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { default as cx } from 'classnames'
 import { button, div, h, i, img } from 'react-hyperscript-helpers'
+import { TransitionMotion, spring } from 'react-motion'
 
 import { toggleAlbums, fetchAlbums, playId } from '../actions'
 import { getCoverURL } from './Cover'
 
-const _Album = ({ album, currentAlbum, playId }) => {
+const _Album = ({ album, currentAlbum, playId, style }) => {
 	const cn = cx('album', { 'current-album': currentAlbum === album.title })
 
 	return (
-		div({ className: cn, onClick: () => playId(album.songs[0].id) }, [
+		div({ className: cn, style, onClick: () => playId(album.songs[0].id) }, [
 			img('.cover-art', { src: getCoverURL(album.songs[0]), alt: 'cover' }),
 			div('.album-title', album.title),
 			div('.album-date', album.date) ])
@@ -31,17 +32,36 @@ class Albums extends React.Component {
 	}
 
 	render () {
-		if (!this.props.albums) return null
+		const albums = this.props.albums || []
+		const expandWidth = 100
+		const shrinkWidth = this.props.animation ? 0 : expandWidth
+
+		// TransitionMotion
+		const willEnter = () => ({ width: shrinkWidth })
+		const willLeave = () => ({ width: spring(shrinkWidth) })
+
+		const defaultStyles = albums.map((a) => ({
+			key: a.title,
+			style: { width: shrinkWidth },
+			data: a
+		}))
+
+		const styles = albums.map((a) => ({
+			key: a.title,
+			style: { width: spring(expandWidth) },
+			data: a
+		}))
 
 		return (
-			div('.albums', [ this.props.albums.map((a) =>
-				h(Album, { key: a.title, album: a, currentAlbum: this.props.song.album })),
-				button('.material-button.close-albums', { onClick: () => this.props.toggleAlbums() }, [
-					i('.material-icons', 'close') ]) ])
+			h(TransitionMotion, { willEnter, willLeave, styles, defaultStyles }, [(iStyles) =>
+				div('.albums', {key: 'albums'}, [ iStyles.map(({ key, style, data }) =>
+					h(Album, { key, style, album: data, currentAlbum: this.props.song.album })),
+					button('.material-button.close-albums', { onClick: () => this.props.toggleAlbums() }, [
+						i('.material-icons', 'close') ]) ]) ])
 		)
 	}
 }
 
-const mapStateToProps = (state) => ({ albums: state.mpc.albums })
+const mapStateToProps = (state) => ({ albums: state.mpc.albums, animation: state.ui.animation })
 
 export default connect(mapStateToProps, { toggleAlbums, fetchAlbums })(Albums)
--- src/client/js/reducers/ui.js
+++ src/client/js/reducers/ui.js
@@ -2,7 +2,8 @@ import { FLIP, TOGGLE_ALBUMS } from '../actions/'
 
 const initialState = {
 	albums: false, // bottom panel opened?
-	flipped: false // songs list visible?
+	flipped: false, // songs list visible?
+	animation: false // disable for low end devices
 }
 
 export default (state = initialState, action) => {
