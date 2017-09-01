64ec7315eb4c549af8a2ca0f99682610685e2055
Delapouite <delapouite@gmail.com>
2017-09-01 18:22:10 +0200

    refactor(Albums): add separate renderButtons

--- src/client/js/components/Albums.js
+++ src/client/js/components/Albums.js
@@ -42,6 +42,28 @@ class Albums extends React.Component {
 		this.props.fetchAlbums(song, tag)
 	}
 
+	renderButtons () {
+		return div('.buttons', [
+			span(`${this.props.albums.length} albums`),
+			this.props.tag !== 'date' ? null : span(this.date),
+			this.props.tag !== 'date' ? null : button('.material-button',
+				{ onClick: () => this.fetchAlbums({...this.props.song, date: this.date - 1 }, 'date') }, [
+				i('.material-icons', 'keyboard_arrow_left') ]),
+			this.props.tag !== 'date' ? null : button('.material-button',
+				{ onClick: () => this.fetchAlbums({...this.props.song, date: this.date + 1 }, 'date') }, [
+				i('.material-icons', 'keyboard_arrow_right') ]),
+			this.props.rows <= 1 ? null : button('.material-button',
+				{ onClick: () => this.props.changeRows(-1) }, [
+				i('.material-icons', 'vertical_align_bottom') ]),
+			button('.material-button',
+				{ onClick: () => this.props.changeRows(1) }, [
+				i('.material-icons', 'vertical_align_top') ]),
+			button('.material-button',
+				{ onClick: () => this.props.toggleAlbums(null, false) }, [
+				i('.material-icons', 'close') ])
+		])
+	}
+
 	render () {
 		const albums = this.props.albums || []
 
@@ -79,30 +101,14 @@ class Albums extends React.Component {
 			style: { height: toggleHeight }
 		}
 
+		const buttons = this.props.show ? this.renderButtons() : null
+
 		return (
 			h(TransitionMotion, transitionProps, [(transitionStyles) =>
 				h(Motion, motionProps, [(motionStyle) =>
 					div('.albums', {key: 'albums', style: motionStyle}, [ transitionStyles.map(({ key, style, data }) =>
 						h(Album, { key, style, tag: this.props.tag, album: data, currentAlbum: this.props.song.album })),
-						! this.props.show ? null : div('.buttons', [
-							span(`${this.props.albums.length} albums`),
-							this.props.tag !== 'date' ? null : span(this.date),
-							this.props.tag !== 'date' ? null : button('.material-button',
-								{ onClick: () => this.fetchAlbums({...this.props.song, date: this.date - 1 }, 'date') }, [
-								i('.material-icons', 'keyboard_arrow_left') ]),
-							this.props.tag !== 'date' ? null : button('.material-button',
-								{ onClick: () => this.fetchAlbums({...this.props.song, date: this.date + 1 }, 'date') }, [
-								i('.material-icons', 'keyboard_arrow_right') ]),
-							this.props.rows <= 1 ? null : button('.material-button',
-								{ onClick: () => this.props.changeRows(-1) }, [
-								i('.material-icons', 'vertical_align_bottom') ]),
-							button('.material-button',
-								{ onClick: () => this.props.changeRows(1) }, [
-								i('.material-icons', 'vertical_align_top') ]),
-							button('.material-button',
-								{ onClick: () => this.props.toggleAlbums(null, false) }, [
-								i('.material-icons', 'close') ])
-						])
+						buttons,
 					])
 				])
 			])
